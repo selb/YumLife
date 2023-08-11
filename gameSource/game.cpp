ceb2c742cea91af9cd8fc471b7089537c3e4d3a0
Brandon Bensel <benselb@selb.us>
2023-08-11 16:38:39 -0400

    Update clientTag.

--- gameSource/game.cpp
+++ gameSource/game.cpp
@@ -9,7 +9,7 @@ const char *yumSubVersion = "";
 // Please use this tag to describe your client honestly and uniquely
 // client_official is reserved for the unmodded client
 // do not include whitespace in your tag
-const char *clientTag = "client_hetuw";
+const char *clientTag = "client_yumlife";
 
 
 
