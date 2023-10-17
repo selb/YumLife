c2198463ba24209dd6331365b7d1d1e89e1c36b7
TheMingo888 <TheMingo888@users.noreply.github.com>
2023-10-17 14:38:37 -0700

    Test twinning without disconnecting

--- gameSource/TwinPage.cpp
+++ gameSource/TwinPage.cpp
@@ -209,7 +209,7 @@ void TwinPage::actionPerformed( GUIComponent *inTarget ) {
 
         SettingsManager::setSetting( "twinCode", userTwinCode );
 
-        userTwinCount = mPlayerCountRadioButtonSet->getSelectedItem() + 2;
+        userTwinCount = mPlayerCountRadioButtonSet->getSelectedItem() + 1;
 
         setSignal( "done" );
         }
