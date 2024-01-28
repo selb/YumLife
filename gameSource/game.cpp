25cf2cdf3cfc99c851b85b7e96d68b9f745a1b10
Brandon Bensel <benselb@selb.us>
2024-01-28 16:24:21 -0500

    bump yumSubVersion

--- gameSource/game.cpp
+++ gameSource/game.cpp
@@ -3,7 +3,7 @@ int dataVersionNumber = 0;
 
 int binVersionNumber = versionNumber;
 
-const char *yumSubVersion = ".5";
+const char *yumSubVersion = ".6";
 
 // Note to modders:
 // Please use this tag to describe your client honestly and uniquely
