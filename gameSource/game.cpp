0ecf2d72512360fe52b97d30e4705dc5551dc7df
Brandon Bensel <benselb@selb.us>
2026-05-09 18:13:51 -0500

    bump expected data version, clear yumSubVersion

--- gameSource/game.cpp
+++ gameSource/game.cpp
@@ -3,12 +3,12 @@ int dataVersionNumber = 0;
 
 int binVersionNumber = versionNumber;
 
-const char *yumSubVersion = ".3";
+const char *yumSubVersion = "";
 // The last data version number released _before_ this client version. Make sure
 // to update this with every upstream versionNumber bump! Tracking this allows
 // us to accurately apply data updates when users "leapfrog" a data update with
 // a newer version of the client.
-int yumExpectedDataVersionNumber = 433;
+int yumExpectedDataVersionNumber = 435;
 
 // Note to modders:
 // Please use this tag to describe your client honestly and uniquely
